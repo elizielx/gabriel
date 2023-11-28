4027ee22e0965f6e4079325f61d1f0d83d21717f
elizielx <yehezkieldio35@gmail.com>
2023-11-28 08:19:52 +0800

    feat(gabriel-web): temp trpc impl

--- apps/gabriel-web/src/routes/dashboard/index.tsx
+++ apps/gabriel-web/src/routes/dashboard/index.tsx
@@ -1,11 +1,24 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { useAuthSession, useAuthSignout } from "../plugin@auth";
-import { Form } from "@builder.io/qwik-city";
+import { Form, routeLoader$ } from "@builder.io/qwik-city";
+import { client } from "../../trpc-client";
+
+export const useHealth = routeLoader$(async () => {
+    const health = (await client.health.health.query()).status;
+    if (health === "ok") {
+        return {
+            health,
+        };
+    } else {
+        throw new Error("API is down");
+    }
+});
 
 export default component$(() => {
     const session = useAuthSession();
     const signOut = useAuthSignout();
+    const health = useHealth();
 
     return (
         <>
@@ -13,6 +26,8 @@ export default component$(() => {
                 <h1>Dashboard</h1>
                 <br />
                 <p>Welcome, {session.value?.user?.name}</p>
+                <br />
+                <p>API is {health.value.health}</p>
                 <br />
                 <Form action={signOut}>
                     <input type="hidden" name="callbackUrl" value="/" />
