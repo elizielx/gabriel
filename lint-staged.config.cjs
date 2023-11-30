86fd2fafcd97627100bec7fa85ae9b52a2a0efac
elizielx <yehezkieldio35@gmail.com>
2023-11-30 09:19:00 +0800

    chore: lint-staged

---
+++ lint-staged.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    "{apps,libs,tools}/**/*.{ts,tsx}": (files) => {
+        return `nx affected --target=lint --files=${files.join(",")}`;
+    },
+    "{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json}": [
+        (files) => `nx affected:lint --files=${files.join(",")}`,
+        (files) => `nx format:write --files=${files.join(",")}`,
+        (files) => `nx affected --target=build --files=${files.join(",")}`,
+    ],
+};
