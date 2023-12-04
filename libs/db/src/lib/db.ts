0c673a13e02a0f5f079599538562887e6105f7aa
elizielx <yehezkieldio35@gmail.com>
2023-12-05 06:25:53 +0800

    chore: broken stuff

--- libs/db/src/lib/db.ts
+++ libs/db/src/lib/db.ts
@@ -6,5 +6,5 @@ const client = postgres(process.env.POSTGRES_CONNECTION_STRING as string);
 export const db = drizzle(client);
 
 if (process.env["NODE_ENV"] !== "production") {
-    migrate(db, { migrationsFolder: "./libs/db/src/lib/drizzle" });
+    migrate(db, { migrationsFolder: "libs/db/src/lib/drizzle" });
 }
--- libs/trpc-client/src/lib/trpc-client.ts
+++ libs/trpc-client/src/lib/trpc-client.ts
@@ -4,6 +4,7 @@ import superjson from "superjson";
 import { getApiBaseUrl } from "@gabriel/shared";
 import type { AppRouter } from "@gabriel/trpc";
 
+console.log("getApiBaseUrl", `${getApiBaseUrl()}/trpc`);
 export const client = createTRPCProxyClient<AppRouter>({
     transformer: superjson,
     links: [
